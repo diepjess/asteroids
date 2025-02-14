e9a3ed091ff70de7293b1f7aeac5c8e211f47f34
diepjess <jessicadiep97@gmail.com>
2025-02-14 17:48:31 -0500

    feat: Add game Clock

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ from constants import *
 
 def main():
     pygame.init()
-    game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # fill screen with color
-        game_screen.fill("black")
+        screen.fill("black")
         
         # flip() the display to refresh screen
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 
