f6f16325849e6212e0e79b8d220bb533fb75f6b8
diepjess <jessicadiep97@gmail.com>
2025-02-14 17:27:53 -0500

    feat: Add skeleton game loop logic and initialize basic pygame surface

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # fill screen with color
+        game_screen.fill("black")
+        
+        # flip() the display to refresh screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
