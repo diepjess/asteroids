70da61d24bb55f0fad67aea8d4e8eba144be0e1d
diepjess <jessicadiep97@gmail.com>
2025-02-17 15:15:10 -0500

    feat: Add Asteroid and AsteroidField class. Add asteroids to relevant containers

---
+++ asteroid.py
@@ -0,0 +1,26 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    def draw(self, screen):
+        """Draw asteroid represented as a circle
+
+        Args:
+            screen (pygame.Surface): the Surface to draw on
+
+        Returns:
+            pygame.Rect: Rect bounding the changed pixels
+        """
+        return pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        """Update position of asteroid to move in straight line
+
+        Args:
+            dt (float): delta time since last frame drawn
+        """
+        self.position += (self.velocity * dt)
