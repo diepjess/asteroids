32ae74a7bf8ad450c760f5560866702e3c767718
diepjess <jessicadiep97@gmail.com>
2025-02-17 16:22:58 -0500

    feat: Add rate limit for player shooting

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     # in the player class, provided by Boot.dev
     def triangle(self):
@@ -46,6 +47,7 @@ class Player(CircleShape):
         Args:
             dt (float): delta time since last frame drawn
         """
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_w]:
@@ -64,5 +66,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
